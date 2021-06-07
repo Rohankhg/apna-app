feff79f3ced1aaac8ec94bafa7626f76d20c31c5
ROHAN <01fe18bcs175@kletech.ac.in>
2021-06-07 18:47:08 +0530

    commiting

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        "BASE_URL": "https://naya-app-kle.herokuapp.com/",
+        "MONGODB_URL": "mongodb+srv://kle_dcc:kle_dcc@dcc.oawat.mongodb.net/suga?retryWrites=true&w=majority",
+        "ACCESS_TOKEN_SECRET": "rqqohan",
+        "REFRESH_TOKEN_SECRET": "rqqohan1",
+        "PAYPAL_CLIENT_ID": "AcMFqHWUMiyIHV83YmPy1P2nef5vTeFca8tznbmpnS16gEj8apTG3V0o9I-vkXRiJEgbesKU3a8TvcO_",
+        "CLOUD_UPDATE_PRESET": "",
+        "CLOUD_NAME": "",
+        "CLOUD_API": ""
+    }
+}
